d96592308c1d49cd444a2736ca879a15b726cefe
Ярослав Меньшиков <menshikov.yaroslav@gmail.com>
2021-09-18 15:17:28 +0700

    Test method of parsing controller

---
+++ Backend/parsing_api/parsing_api/Controllers/ParsingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace parsing_api.Controllers
+{
+    [ApiController]
+    public class ParsingController : ControllerBase
+    {
+        [Route("api/test")]
+        [HttpGet]
+        public ActionResult<string> Test()
+        {
+            return Ok("Test passed");
+        }
+    }
+}
