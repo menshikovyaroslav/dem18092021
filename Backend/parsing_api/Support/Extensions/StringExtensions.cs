0ab68ec381c8c8b84a22e1762e98aa1a698b6395
Ярослав Меньшиков <menshikov.yaroslav@gmail.com>
2021-09-18 18:42:39 +0700

    библиотека support - классы которые будут использоваться из разных проектов.

---
+++ Backend/parsing_api/Support/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Support.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Проверка на пустую строку
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static bool IsEmpty(this string x)
+        {
+            return string.IsNullOrEmpty(x);
+        }
+    }
+}
