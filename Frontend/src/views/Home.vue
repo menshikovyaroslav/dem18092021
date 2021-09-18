d8a547920eca4b812ba9cd024e71ed1f7ef62892
Portulak <ilyshk999@gmail.com>
2021-09-18 18:26:48 +0300

    - Начал получать списки - Отрисовываю списки для выбора

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,7 +1,32 @@
 <template>
   <v-container>
     <v-row class="text-center">
-
+      <v-col
+        cols="4"
+        class="mt-4"
+      >
+        <v-select
+          v-model="chosenData.region"
+          :items="regionList"
+          label="Регион"
+          item-text="name"
+          return-object
+          dense
+        />
+      </v-col>
+      <v-col
+        cols="4"
+        class="mt-4"
+      >
+        <v-select
+          v-model="chosenData.portal"
+          :items="portalList"
+          label="Портал"
+          item-text="name"
+          return-object
+          dense
+        />
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -12,24 +37,36 @@ import axios from 'axios'
 export default {
   name: 'Home',
   data() {
-    return {}
+    return {
+      chosenData: {
+        region: {
+          name: '',
+          id: 0,
+        },
+        portal: {
+          name: '',
+          id: 0,
+          link: '',
+        },
+      },
+      regionList: [],
+      portalList: [],
+    }
   },
   methods: {},
   mounted() {
     axios({
-      method: 'post',
-      url: 'http://94.180.116.89:5522/api/getbynumber',
-      data: {
-        number: '',
-        region: 777,
-      },
+      url: 'http://94.180.116.89:5522/api/regions',
+      method: 'get',
     })
-      .then(res => {
-        console.log(`res`, res)
-      })
-      .catch(err => {
-        console.log(`error`, err)
-      })
+      .then(res => this.regionList = res.data)
+      .catch(error => console.log(`error`, error))
+    axios({
+      url: 'http://94.180.116.89:5522/api/portals',
+      method: 'get',
+    })
+      .then(res => this.portalList = res.data)
+      .catch(error => console.log(`error`, error))
   },
 }
 </script>
