46ec80728a43779c46bc17acac36cb3916f7ad06
Portulak <ilyshk999@gmail.com>
2021-09-18 15:02:51 +0300

    - Удалил лишние роуты, поменял Home на динамический роут

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,20 +7,14 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "homepage" */ '../views/Home.vue')
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
-  routes
+  mode: 'history',
+	base: process.env.BASE_URL,
+  routes,
 })
 
 export default router
