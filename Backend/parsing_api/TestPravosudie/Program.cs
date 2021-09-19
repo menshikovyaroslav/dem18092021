5b360b5b36ac9f73f31a116223637efc98f79381
Ярослав Меньшиков <menshikov.yaroslav@gmail.com>
2021-09-19 10:56:16 +0700

    test selenium

--- Backend/parsing_api/TestPravosudie/Program.cs
+++ Backend/parsing_api/TestPravosudie/Program.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Linq;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Request.Support;
+using Support;
 using Support.Request;
 using System;
 using System.Collections.Generic;
@@ -18,30 +19,13 @@ namespace TestPravosudie
     {
         static void Main(string[] args)
         {
-         //   System.Environment.SetEnvironmentVariable("webdriver.chrome.driver", @"c:\path\to\driver\chromedriver.exe");
             IWebDriver driver = new ChromeDriver();
             driver.Url = @"https://bsr.sudrf.ru/bigs/portal.html";
-            Thread.Sleep(3000);
+            Thread.Sleep(5000);
             driver.FindElement(By.CssSelector(".date-filter-from")).SendKeys(DateTime.Now.AddDays(-30).ToString("d"));
             driver.FindElement(By.CssSelector(".date-filter-to")).SendKeys(DateTime.Now.AddDays(-29).ToString("d"));
             Thread.Sleep(1000);
-            //   driver.FindElements(By.CssSelector(".ui-autocomplete-input"))[5].SendKeys("137");
-
-            var els = driver.FindElements(By.CssSelector(".ui-autocomplete-input"));
-            var i = 0;
-            foreach (var el in els)
-            {
-                el.SendKeys(i.ToString());
-                i++;
-            }
-
-            driver.FindElement(By.XPath(@".//input[@id='searchFormButton']")).Click();
-            Thread.Sleep(3000);
-
-            var html = driver.PageSource;
-
-            Console.WriteLine(html);
-
+            driver.FindElement(By.XPath("//input[@placeholder='Введите номер дела']")).SendKeys("137");
 
             Console.ReadKey();
         }
