48d2699b7ca114697d8d8236a94e98a4e0c55b2c
Kezhich <azerot96@g-mail.com>
2021-09-19 14:14:30 +0300

    cases table and top stats layout added

--- Frontend/src/components/CasesTable.vue
+++ Frontend/src/components/CasesTable.vue
@@ -1,25 +1,27 @@
 <template>
-	<div class="jobs-table">
-		<h2>Список задач на парсинг</h2>
-		<v-data-table
-			@click:row="handleRowClick"
-			hide-default-footer
-			:loading="loadingList"
-			loading-text="Загружаются данные о запросах..."
-			:headers="headers"
-			:items="jobsList"
-			:items-per-page="Infinity"
-			:item-class="() => 'jobs-table__row'"
-			locale="ru-RU"
-		>
-		</v-data-table>
+	<div class="cases-table">
+		<div class="cases-table__table">
+			<h2>Список дел</h2>
+			<v-data-table
+				@click:row="handleRowClick"
+				hide-default-footer
+				:loading="loadingList"
+				loading-text="Загружаются данные о запросах..."
+				:headers="headers"
+				:items="jobsList"
+				:items-per-page="Infinity"
+				:item-class="() => 'jobs-table__row'"
+				locale="ru-RU"
+			>
+			</v-data-table>
+		</div>
 	</div>
 </template>
 
 <script>
 	import { mapState, mapActions } from "vuex";
 	export default {
-		name: "JobsTable",
+		name: "CasesTable",
 		data: () => ({
 			loadingList: true,
 			headers: [
