b2ebeb55bf8bfc50e35bbb0e4638710a9e6d865b
Kezhich <azerot96@g-mail.com>
2021-09-19 03:28:30 +0300

    Jobs table created

--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -1,52 +1,68 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Api from '@api/index';
+import Api from '@api/index'
 // import bus from '@/utils/bus.plugin'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    isAppReady: false,
-    regionsList: [],
-    portalsList:[]
-  },
-  mutations: {
-    SET_STATE(state, { key, value }) {
-      state[key] = value;
-    },
-  
-  },
-  actions: {
-    async initiateApp({commit, dispatch}){
-      try {
-        const responses = await dispatch("getPortalsList")
-        if(responses) {
-          commit('SET_STATE', {key: 'isAppReady', value: true})
-        }
-      } catch(e) {
-        console.warn(e)
-      }
-    },
+	state: {
+		isAppReady: false,
+		regionsList: [],
+		portalsList: [],
+		jobsList: []
+	},
 
-   async  getPortalsList({commit}) {
-      try {
-        const stateKeysList = ["regionsList", "portalsList"]
-        const list = [Api.getRegionsList(), Api.getPortalsList()]
+	mutations: {
+		SET_STATE(state, { key, value }) {
+			state[key] = value
+		}
+	},
 
-        const responses = await Promise.all(list);
-          responses.forEach((res, index) =>
-            commit('SET_STATE', {
-              key: stateKeysList[index],
-              value: res,
-            }),
-          );
-          return responses
-      } catch(e) {
-        console.warn(e)
-      }
-    }
-  },
-  modules: {
-  }
+	actions: {
+		async initiateApp({ commit }) {
+			try {
+				const stateKeysList = ['regionsList', 'portalsList']
+				const list = [Api.getRegionsList(), Api.getPortalsList()]
+
+				const responses = await Promise.all(list)
+				responses.forEach((res, index) =>
+					commit('SET_STATE', {
+						key: stateKeysList[index],
+						value: res
+					})
+				)
+			} catch (e) {
+				console.warn(e)
+			} finally {
+				commit('SET_STATE', { key: 'isAppReady', value: true })
+			}
+		},
+
+		async getJobsList({ state, commit }) {
+			const parseDate = date =>
+				new Date(date).toLocaleString('ru-RU', {
+					year: 'numeric',
+					month: 'long',
+					day: 'numeric'
+				})
+			try {
+				const jobsList = await Api.getJobsList()
+
+				const parsedJobsList = jobsList.map(item => ({
+					...item,
+					dateCreated: parseDate(item.time),
+					dateStart: parseDate(item.timeForm),
+					dateEnd: parseDate(item.timeTo),
+					portalName: state.portalsList.find(
+						portalItem => portalItem.id === item.portalId
+					).name
+				}))
+
+				commit('SET_STATE', { key: 'jobsList', value: parsedJobsList })
+			} catch (e) {
+				console.warn(e)
+			}
+		}
+	}
 })
