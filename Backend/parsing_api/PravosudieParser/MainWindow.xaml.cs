e874cfa3de4448f9a843c0a45db5a22282c1af3e
Ярослав Меньшиков <menshikov.yaroslav@gmail.com>
2021-09-19 11:26:52 +0700

    установка selenium, перенос кода из тест проекта

--- Backend/parsing_api/PravosudieParser/MainWindow.xaml.cs
+++ Backend/parsing_api/PravosudieParser/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +26,17 @@ namespace PravosudieParser
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartParser_Click(object sender, RoutedEventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = @"https://bsr.sudrf.ru/bigs/portal.html";
+            Thread.Sleep(5000);
+            driver.FindElement(By.CssSelector(".date-filter-from")).SendKeys(DateTime.Now.AddDays(-30).ToString("d"));
+            driver.FindElement(By.CssSelector(".date-filter-to")).SendKeys(DateTime.Now.AddDays(-29).ToString("d"));
+            Thread.Sleep(1000);
+            driver.FindElement(By.XPath("//input[@placeholder='Введите номер дела']")).SendKeys("137");
         }
     }
 }
